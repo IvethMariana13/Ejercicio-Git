da3b75d0fba07be74e6f84e8b1293364e4b4f37a
IvethMariana13 <iveth.serna.13@gmail.com>
2022-04-12 21:14:17 -0500

    My first commit

---
+++ EjercicioGit/Person.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EjercicioGit
+{
+    public class Person
+    {
+        public void Persona()
+        {
+            Console.WriteLine("I'm Person");
+        }
+    }
+}
